1d62289b2ff97ad8407df2b6b221555b96fa5ed9
Benoit <benoit@superhuit.ch>
2017-12-06 19:05:26 +0100

    improve code

--- js/homepage.js
+++ js/homepage.js
@@ -1,14 +1,9 @@
-var button    = document.getElementsByClassName("button");
-var homeTitle = document.getElementsByClassName("home-title");
-
 function scrollDown() {
-  console.log(button);
-  console.log(homeTitle);
-  document.getElementById("button").classList.add("is-hidden");
+  document.getElementById("play-button").classList.add("is-hidden");
   document.getElementById("home-title").classList.add("is-hidden");
   document.getElementById("background").classList.add("is-scrolled");
   setTimeout( function hiddenHome() {
-    document.getElementById("button").classList.add("is-display-none");
+    document.getElementById("play-button").classList.add("is-display-none");
     document.getElementById("home-title").classList.add("is-display-none");
     document.getElementById("level-title").classList.remove("is-display-none");
     document.getElementById("button-level-container").classList.remove("is-display-none");
@@ -28,7 +23,7 @@ function scrollUp() {
   document.getElementById("button-level-container").classList.add("is-hidden");
   document.getElementById("back-button").classList.add("is-hidden");
   setTimeout( function hiddenLevel() {
-    document.getElementById("button").classList.remove("is-display-none");
+    document.getElementById("play-button").classList.remove("is-display-none");
     document.getElementById("home-title").classList.remove("is-display-none");
     document.getElementById("level-title").classList.add("is-display-none");
     document.getElementById("button-level-container").classList.add("is-display-none");
@@ -36,7 +31,7 @@ function scrollUp() {
   }, 500);
 
   setTimeout( function showHome() {
-    document.getElementById("button").classList.remove("is-hidden");
+    document.getElementById("play-button").classList.remove("is-hidden");
     document.getElementById("home-title").classList.remove("is-hidden");
     document.getElementById("level-title").classList.add("is-display-none");
     document.getElementById("button-level-container").classList.add("is-display-none");
