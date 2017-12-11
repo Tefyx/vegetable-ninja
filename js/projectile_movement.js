ffa5d3bf8e56f960216aee7c072de2be3c2c2004
Richard Aurélien <aurelien.r@hotmail.ch>
2017-12-11 16:42:43 +0100

    New shuriken Animation + infinite shuriken
    Les shurikens sont crées sur le moment
    et ils se lancent dans la direction du dernier mouvement du personnage

--- js/projectile_movement.js
+++ js/projectile_movement.js
@@ -1,27 +1,30 @@
-
 direction = 0;
 
 function start() {
-  var elem = document.getElementById("shuriken");
+  //var elem = document.getElementById("shuriken");
+  var elem = document.createElement("IMG");
+  elem.src = "../img/Weapons/shuriken.png";
+  elem.className = "shuriken";
   elem.direction = direction;
-  console.log(elem.direction);
-  var pos = 0;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 300) {
-      clearInterval(id);
-    } else {
-      if (elem.direction == 119) {
-        elem.style.top = -pos + 'px';
-      }else if (elem.direction == 97) {
-        elem.style.left = -pos + 'px';
-      }else if (elem.direction == 100) {
-        elem.style.left = pos + 'px';
-      }else if (elem.direction == 115) {
-        elem.style.top = pos + 'px';
-      }
-      pos++;
+  elem.pos = 0;
+  container.appendChild(elem);
+}
+
+var id = setInterval(frame, 10);
+
+function frame() {
+  for (var i = 0; i < container.children.length; i++) {
+    var elem = container.children[i];
+    if (elem.direction == 119) {
+      elem.style.top = -elem.pos + 'px';
+    }else if (elem.direction == 97) {
+      elem.style.left = -elem.pos + 'px';
+    }else if (elem.direction == 100) {
+      elem.style.left = elem.pos + 'px';
+    }else if (elem.direction == 115) {
+      elem.style.top = elem.pos + 'px';
     }
+    elem.pos = elem.pos + 5;
   }
 }
 
