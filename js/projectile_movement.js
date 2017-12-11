7c8ddd2f04330f3b9fdf36dd23e3cee7ab10fe21
Richard Aurélien <aurelien.r@hotmail.ch>
2017-12-11 15:56:27 +0100

    Shuriken movements based on last key pressed

--- js/projectile_movement.js
+++ js/projectile_movement.js
@@ -1,1 +1,45 @@
 
+direction = 0;
+
+function start() {
+  var elem = document.getElementById("shuriken");
+  elem.direction = direction;
+  console.log(elem.direction);
+  var pos = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 300) {
+      clearInterval(id);
+    } else {
+      if (elem.direction == 119) {
+        elem.style.top = -pos + 'px';
+      }else if (elem.direction == 97) {
+        elem.style.left = -pos + 'px';
+      }else if (elem.direction == 100) {
+        elem.style.left = pos + 'px';
+      }else if (elem.direction == 115) {
+        elem.style.top = pos + 'px';
+      }
+      pos++;
+    }
+  }
+}
+
+
+window.onkeypress = function(event) {
+  if (event.keyCode == 100) {
+    direction = event.keyCode;
+  }
+  if (event.keyCode == 119) {
+    direction = event.keyCode;
+  }
+  if (event.keyCode == 97) {
+    direction = event.keyCode;
+  }
+  if (event.keyCode == 115) {
+    direction = event.keyCode;
+  }
+   if (event.keyCode == 112) {
+     start();
+   }
+}
