80df11e9efc9a8ba06fe8b4661c3fca2e624ad3e
Benoit <benoit@superhuit.ch>
2017-12-11 16:03:08 +0100

    integrate level

--- js/homepage.js
+++ js/homepage.js
@@ -44,6 +44,39 @@ function showLevel() {
     document.getElementById("level-modal").classList.remove("is-hidden");
     document.getElementById("level-modal").classList.add("modal-show");
   }, 25);
+
+  setTimeout( function createMap() {
+    var thisMap =
+      ['B','B','B','B','B','B','B','B','B','B','B','B','B','B','B','B',
+      'B','S','S','S','S','S','S','S','S','S','S','P','C','P','S','B',
+      'B','S','S','R','R','S','P','P','P','P','P','P','P','P','S','B',
+      'B','S','S','R','S','S','P','P','P','P','P','P','P','P','S','B',
+      'B','R','S','S','S','S','P','P','P','P','P','P','P','P','S','B',
+      'B','R','R','S','S','S','P','P','P','S','S','S','S','R','S','B',
+      'B','S','S','S','S','S','P','P','P','S','S','S','S','S','S','B',
+      'L','L','L','L','L','L','P','P','P','L','L','L','L','L','L','L',
+      'L','L','L','L','L','L','P','P','P','L','L','L','L','L','L','L',
+      'L','L','L','L','L','L','P','P','P','L','L','L','L','L','L','L',
+      'B','S','S','S','S','S','P','P','P','S','S','S','S','S','S','B',
+      'B','S','S','R','S','S','P','P','P','S','S','R','S','S','S','B',
+      'B','S','S','S','R','S','P','P','P','S','S','S','S','S','S','B',
+      'B','S','R','S','R','S','P','P','P','S','S','S','R','S','S','B',
+      'B','S','S','R','S','S','P','P','P','S','S','S','S','S','S','B',
+      'B','B','B','B','B','B','B','B','B','B','B','B','B','B','B','B'];
+    for (var i = 0; i < thisMap.length; i++) {
+    var elem = document.createElement("img");
+    elem.src="img/textures/" + thisMap[i]+".png";
+    elem.style.height="6.25%";
+    elem.style.width="6.25%";
+    elem.style.float="left";
+    document.getElementById("level").appendChild(elem);
+    }
+  }, 100);
+
+  setTimeout( function showMap() {
+    document.getElementById("level").classList.remove("is-hidden");
+  }, 500);
+
   setTimeout( function showExitButton() {
     document.getElementById("exit-button").classList.remove("is-hidden");
   }, 1000);
@@ -55,5 +88,7 @@ function exitLevel() {
   setTimeout( function closeModal() {
     document.getElementById("level-modal").classList.add("is-display-none");
     document.getElementById("exit-button").classList.add("is-hidden");
+    document.getElementById("level").classList.add("is-hidden");
+    document.getElementById("level").innerHTML = "";
   }, 500);
 }
