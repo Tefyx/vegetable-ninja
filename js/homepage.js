5b9112bf933253912b2cce9119022368c2362487
Richard Aurélien <aurelien.r@hotmail.ch>
2018-01-11 20:38:17 +0100

    Revert "Revert "mew2 can be killed and we cannot hurt ourselves anymore""
    This reverts commit e6e96286d1a87dea59571065f06f1739c1dd4cae.

--- js/homepage.js
+++ js/homepage.js
@@ -13,8 +13,8 @@ let hero;
 let direction;
 var weapon = "shuriken";
 var fired = false;
-var id = setInterval(frame, 10);
-setInterval(ennemy_fire, 1500);
+var ennemy_fireInterval;
+var frameInterval;
 
 // Init variables
 function init() {
@@ -151,6 +151,8 @@ function showLevel(level_id) {
     mew.posy = 6;
     mew.traversable = false;
     document.getElementById("div-mew").appendChild(mew);
+    frameInterval = setInterval(frame, 10);
+    ennemy_fireInterval = setInterval(ennemy_fire, 1500);
   }, 100);
 
   setTimeout( function showMap() {
@@ -258,13 +260,14 @@ function frame() {
           disappear(elem.offsetLeft, elem.offsetTop,elem.className,"wall_hit");
           level.removeChild(elem);
         }
-        if(elem.posy < hero.posy*48 + 48 && elem.posy > hero.posy*48 && elem.posx < hero.posx*48 && elem.posx > hero.posx*48 -48) {
+        if(elem.posy < hero.posy*48 + 48 && elem.posy > hero.posy*48 && elem.posx < hero.posx*48 && elem.posx > hero.posx*48 -48 && elem.className =="carotte") {
           disappear(elem.offsetLeft, elem.offsetTop,elem.className,"hero_hit");
           level.removeChild(elem);
         }
 
         if(elem.posy < mew.posy*48 + 48 && elem.posy > mew.posy*48 && elem.posx < mew.posx*48 + 48 && elem.posx > mew.posx*48 && elem.className !="carotte") {
           disappear(elem.offsetLeft, elem.offsetTop,elem.className,"mew_hit");
+          clearInterval(ennemy_fireInterval);
           level.removeChild(elem);
           document.getElementById("div-mew").removeChild(mew);
         }
@@ -295,7 +298,9 @@ function disappear(x, y,className, action) {
     console.log("YO");
     disappear.src = "./img/Effects/fire.gif";
     level.removeChild(mew);
-  } else {
+  } else if (className !="carotte" && action == "hero_hit") {
+    return;
+  }else {
     disappear.src = "./img/Effects/disappear.gif";
   }
   disappear.style.height = "30px";
