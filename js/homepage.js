d325dac88913ad88a1193fda868ec9299487e084
Orlaurack <sewer.laurent@hotmail.com>
2018-01-14 22:10:51 +0100

    test
    blabla

--- js/homepage.js
+++ js/homepage.js
@@ -97,62 +97,131 @@ function showLevel(level_id) {
 
     if (level_id == 1){
       map = [
-      'R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png',
-      'R.png','S.png','S.png','S.png','S.png','S.png','S.png','S.png','S.png','S.png','S.png','PTL.png','C.png','PTR.png','S.png','R.png',
-      'R.png','S.png','S.png','R.png','R.png','S.png','PTL.png','PT.png','PT.png','PT.png','PT.png','P.png','P.png','PR.png','S.png','R.png',
-      'R.png','S.png','S.png','R.png','S.png','S.png','PL.png','P.png','P.png','P.png','P.png','P.png','P.png','PR.png','S.png','R.png',
-      'R.png','R.png','S.png','S.png','S.png','S.png','PL.png','P.png','P.png','PB.png','PB.png','PB.png','PB.png','PBR.png','S.png','R.png',
-      'R.png','R.png','R.png','S.png','S.png','S.png','PL.png','P.png','PR.png','S.png','S.png','S.png','S.png','R.png','S.png','R.png',
-      'R.png','S.png','S.png','S.png','S.png','S.png','PL.png','P.png','PR.png','S.png','S.png','S.png','S.png','S.png','S.png','R.png',
-      'LT.gif','LT.gif','LT.gif','LT.gif','LT.gif','LT.gif','Bridge1.gif','Bridge2.png','Bridge3.gif','LT.gif','LT.gif','LT.gif','LT.gif','LT.gif','LT.gif','LT.gif',
-      'L.gif','L.gif','L.gif','L.gif','L.gif','L.gif','Bridge4.png','Bridge5.png','Bridge6.gif','L.gif','L.gif','L.gif','L.gif','L.gif','L.gif','L.gif',
-      'LB.gif','LB.gif','LB.gif','LB.gif','LB.gif','LB.gif','Bridge7.gif','Bridge8.png','Bridge9.gif','LB.gif','LB.gif','LB.gif','LB.gif','LB.gif','LB.gif','LB.gif',
-      'R.png','S.png','S.png','S.png','S.png','S.png','PL.png','P.png','PR.png','S.png','S.png','S.png','S.png','S.png','S.png','R.png',
-      'R.png','S.png','S.png','R.png','S.png','S.png','PL.png','P.png','PR.png','S.png','S.png','R.png','S.png','S.png','S.png','R.png',
-      'R.png','S.png','S.png','S.png','R.png','S.png','PL.png','P.png','PR.png','S.png','S.png','S.png','S.png','S.png','S.png','R.png',
-      'R.png','S.png','R.png','S.png','R.png','S.png','PL.png','P.png','PR.png','S.png','S.png','S.png','R.png','S.png','S.png','R.png',
-      'R.png','S.png','S.png','R.png','S.png','S.png','PBL.png','PB.png','PBR.png','S.png','S.png','S.png','S.png','S.png','S.png','R.png',
-      'R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png','R.png']
+      'R' ,'R' ,'R' ,'R' ,'R' ,'R' ,'R','R','R','R','R','R','R','R','R','R',
+      'R' ,'S' ,'S' ,'S' ,'S' ,'S' ,'S','S','S','S','S','PTL','C','PTR','S','R',
+      'R' ,'S' ,'S' ,'R' ,'R' ,'S' ,'PTL','PT','PT','PT','PT','P','P','PR','S','R',
+      'R' ,'S' ,'S' ,'R' ,'S' ,'S' ,'PL','P','P','P','P','P','P','PR','S','R',
+      'R' ,'R' ,'S' ,'S' ,'S' ,'S' ,'PL','P','P','PB','PB','PB','PB','PBR','S','R',
+      'R' ,'R' ,'R' ,'S' ,'S' ,'S' ,'PL','P','PR','S','S','S','S','R','S','R',
+      'R' ,'S' ,'S' ,'S' ,'S' ,'S' ,'PL','P','PR','S','S','S','S','S','S','R',
+      'LT','LT','LT','LT','LT','LT','Bridge1','Bridge2','Bridge3','LT','LT','LT','LT','LT','LT','LT',
+      'L' ,'L','L','L','L','L','Bridge4','Bridge5','Bridge6','L','L','L','L','L','L','L',
+      'LB','LB','LB','LB','LB','LB','Bridge7','Bridge8','Bridge9','LB','LB','LB','LB','LB','LB','LB',
+      'R' ,'S','S','S','S','S','PL','P','PR','S','S','S','S','S','S','R',
+      'R' ,'S','S','R','S','S','PL','P','PR','S','S','R','S','S','S','R',
+      'R' ,'S','S','S','R','S','PL','P','PR','S','S','S','S','S','S','R',
+      'R' ,'S','R','S','R','S','PL','P','PR','S','S','S','R','S','S','R',
+      'R' ,'S','S','R','S','S','PBL','PB','PBR','S','S','S','S','S','S','R',
+      'R' ,'R','R','R','R','R','R','R','R','R','R','R','R','R','R','R']
     startpositionleft = 3;
     startpositiontop = 3;
   }else if (level_id == 2){
     map = [
-    'Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png',
-    'Bush.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','Bush.png',
-    'Bush.png','Tree2_1.png','Tree2_2.png','Tree2_3.png','PGTL.png','PGH.png','PGH.png','PGH.png','PGH.png','PGH.png','PGH.png','PGH.png','PGH.png','PGH.png','G.png','Bush.png',
-    'Bush.png','Tree2_4.png','Tree2_5.png','Tree2_6.png','PGV.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','Bush.png',
-    'Bush.png','Tree2_7.png','Tree2_8.png','Tree2_9.png','PGV.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','Bush.png',
-    'Bush.png','G.png','G.png','G.png','PGV.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','Lac2_3.png','Lac2_4.png','Lac2_5.png','Bush.png',
-    'Bush.png','G.png','G.png','G.png','PGV.png','G.png','G.png','G.png','G.png','G.png','G.png','G.png','Lac2_8.png','W.png','Lac2_10.png','Bush.png',
-    'Bush.png','G.png','G.png','G.png','PGBL.png','PGH.png','PGH.png','PGH.png','PGTR.png','G.png','G.png','G.png','Lac2_13.png','W.png','Lac2_15.png','Bush.png',
-    'Bush.png','G.png','Tree1_1.png','Tree1_2.png','Tree1_3.png','G.png','G.png','G.png','PGV.png','G.png','G.png','G.png','Lac2_18.png','Lac2_19.png','Lac2_20.png','Bush.png',
-    'Bush.png','G.png','Tree1_4.png','Tree1_5.png','Tree1_6.png','G.png','G.png','G.png','G.png','G.png','G.png','Lac2_22.png','Lac2_23.png','Lac2_24.png','G.png','Bush.png',
-    'Bush.png','G.png','Tree1_7.png','Tree1_8.png','Tree1_9.png','G.png','G.png','G.png','G.png','G.png','G.png','Lac2_27.png','W.png','Lac2_29.png','G.png','Bush.png',
-    'Bush.png','G.png','Lac1_2.png','Lac1_3.png','G.png','G.png','G.png','G.png','G.png','G.png','Lac2_31.png','Lac2_32.png','W.png','Lac2_34.png','G.png','Bush.png',
-    'Bush.png','Lac1_6.png','Lac1_7.png','Lac1_8.png','Lac1_9.png','G.png','G.png','G.png','G.png','G.png','Lac2_36.png','W.png','W.png','Lac2_39.png','G.png','Bush.png',
-    'Bush.png','Lac1_11.png','W.png','W.png','Lac1_14.png','Lac1_15.png','G.png','G.png','G.png','G.png','Lac2_41.png','W.png','W.png','Lac2_44.png','Lac2_45.png','Bush.png',
-    'Bush.png','Lac1_16.png','Lac1_17.png','Lac1_18.png','Lac1_19.png','Lac1_20.png','G.png','G.png','G.png','G.png','Lac2_46.png','Lac2_47.png','Lac2_48.png','Lac2_49.png','Lac2_50.png','Bush.png',
-    'Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png','Bush.png']
+    'Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush',
+    'Bush','G','G','G','G','G','G','G','G','G','G','G','G','G','G','Bush',
+    'Bush','Tree2_1','Tree2_2','Tree2_3','PGTL','PGH','PGH','PGH','PGH','PGH','PGH','PGH','PGH','PGR','G','Bush',
+    'Bush','Tree2_4','Tree2_5','Tree2_6','PGV','G','G','G','G','G','G','G','G','G','G','Bush',
+    'Bush','Tree2_7','Tree2_8','Tree2_9','PGV','G','G','G','G','G','G','G','G','G','G','Bush',
+    'Bush','G','G','G','PGV','G','G','G','G','G','G','G','Lac2_3','Lac2_4','Lac2_5','Bush',
+    'Bush','G','G','G','PGV','G','G','G','G','G','G','G','Lac2_8','W','Lac2_10','Bush',
+    'Bush','G','G','G','PGBL','PGH','PGH','PGH','PGTR','G','G','G','Lac2_13','W','Lac2_15','Bush',
+    'Bush','G','Tree1_1','Tree1_2','Tree1_3','G','G','G','PGV','G','G','G','Lac2_18','Lac2_19','Lac2_20','Bush',
+    'Bush','G','Tree1_4','Tree1_5','Tree1_6','G','G','G','PGV','G','G','Lac2_22','Lac2_23','Lac2_24','G','Bush',
+    'Bush','G','Tree1_7','Tree1_8','Tree1_9','G','G','G','PGV','G','G','Lac2_27','W','Lac2_29','G','Bush',
+    'Bush','G','Lac1_2','Lac1_3','G','G','G','G','PGV','G','Lac2_31','Lac2_32','W','Lac2_34','G','Bush',
+    'Bush','Lac1_6','Lac1_7','Lac1_8','Lac1_9','G','G','G','PGV','G','Lac2_36','W','W','Lac2_39','G','Bush',
+    'Bush','Lac1_11','W','W','Lac1_14','Lac1_15','PGL','PGH','PGBR','G','Lac2_41','W','W','Lac2_44','Lac2_45','Bush',
+    'Bush','Lac1_16','Lac1_17','Lac1_18','Lac1_19','Lac1_20','G','G','G','G','Lac2_46','Lac2_47','Lac2_48','Lac2_49','Lac2_50','Bush',
+    'Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush','Bush']
+    startpositionleft = 15;
+    startpositiontop = 3;
+    var raining = document.createElement("img");
+    raining.src="img/textures/rain.png";
+    raining.style.height="100%";
+    raining.style.width="100%";
+
+    raining.style.position="absolute";
+    raining.style.left=0;
+    level.appendChild(raining);
+  }else if (level_id == 3){
+    var map = [
+    'SnR','SnR','SnR','SnR','SnR','SnR','SnWLR','SnR','SnR','SnR','SnR','SnWLR','SnR','SnR','SnR','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','SnWL','Sn','Sn','Sn','Sn','SnWLR','Sn','Sn','SnMushroms1','SnR',
+    'SnR','Sn','Sn','Sn','Snowman1','Sn','SnWL','Sn','Sn','Sn','Sn','SnWL','Sn','SnMushroms1','SnMushroms2','SnR',
+    'SnR','Sn','Sn','Sn','Snowman2','Sn','SnWLR','Sn','Sn','Sn','Sn','SnWLR','Sn','Sn','Sn','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','SnWL','Sn','Sn','Sn','Sn','SnWL','Sn','Sn','Sn','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','SnWL','Sn','Sn','Sn','Sn','SnWLR','Sn','Sn','Sn','SnR',
+    'SnWH','SnWH','SnWH','SnWH','SnWP','SnWH','SnWBL','Sn','Sn','Sn','Sn','SnWL','SnMushroms1','Sn','Sn','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','SnWL','Sn','Sn','Sn','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','SnWL','Sn','Sn','Sn','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','Sn','SnWLT','SnWH','SnWP','SnWH','SnWBL','Sn','Sn','SnMushroms1','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','Sn','SnWL','SnR','Sn','Sn','Sn','Sn','Sn','Sn','SnR',
+    'SnR','Sn','Sn','Sn','Sn','Sn','Sn','SnWL','Sn','Sn','Sn','Sn','Sn','Sn','Sn','SnR',
+    'SnWH','SnWH','SnWP','SnWH','SnWH','SnWH','SnWH','SnWBL','Sn','Sn','Sn','Sn','Sn','Sn','Sn','SnR',
+    'SnR','Sn','Sn','SnMushroms2','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','Sn','SnMushroms2','Sn','SnR',
+    'SnR','Sn','SnMushroms1','Sn','Sn','Sn','Sn','Sn','Sn','SnMushroms2','Sn','Sn','Sn','Sn','Sn','SnR',
+    'SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR','SnR'
+    
+    ]
     startpositionleft = 8;
     startpositiontop = 2;
-  }else{
-    var map = ['R.png']
-    startpositionleft = 8;
-    startpositiontop = 2;
+    var snowing = document.createElement("img");
+    snowing.src="img/textures/snowing.png";
+    snowing.style.height="100%";
+    snowing.style.width="100%";
+
+    snowing.style.position="absolute";
+    snowing.style.left=0;
+    level.appendChild(snowing);
+
+  }else if (level_id == 4){
+    var map = [
+        '1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1',
+        '1','1','1','1','1','0','0','0','0','0','0','0','0','0','0','1',
+        '1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','1','0','1','1','0','1','0','0','0','0','0','0','0','0','1',
+        '1','0','0','0','1','0','1','0','0','0','0','0','0','0','0','1',
+        '1','1','0','0','0','0','1','0','0','0','0','0','0','0','0','1',
+        '1','0','0','0','1','0','1','0','0','0','0','0','0','0','0','1',
+        '1','1','1','1','1','0','1','1','0','0','0','0','0','0','0','1',
+        '1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',
+        '1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'
+        ]
+
+ var simsonHouse = document.createElement("img");
+    simsonHouse.src="img/textures/SimsonHouse.png";
+    simsonHouse.style.height="100%";
+    simsonHouse.style.width="100%";
+simsonHouse.style.zIndex = "-1"; 
+    simsonHouse.style.position="absolute";
+    simsonHouse.style.left=0;
+    level.appendChild(simsonHouse);
+    startpositionleft = 12;
+    startpositiontop = 16;
   }
 
     for (var i = 0; i < map.length; i++) {
       var elem = document.createElement("img");
-      elem.src="img/textures/" + map[i];
+      elem.src="img/textures/" + map[i] + ".png";
       elem.style.height="6.25%";
       elem.style.width="6.25%";
       elem.style.float="left";
-      if(map[i] == "L.gif" || map[i] == "LT.gif" || map[i] == "LB.gif" || map[i] == "B.png" || map[i] == "R.png"){
-         elem.traversable = false;
-       }
-       else{
-         elem.traversable = true;
-       }
+     
+      var traversableList = ['L','LT','LB','B','R','SnR','1'];
+      for(var y = 0; y < traversableList.length; y++) {
+         if(map[i] == ""+traversableList[y]+".png"){
+           elem.traversable = false;
+         }
+         else{
+           elem.traversable = true;
+         }
+      }
       elem.id = "t"+i;
       level.appendChild(elem);
     }
@@ -164,8 +233,8 @@ function showLevel(level_id) {
     hero.style.position="absolute";
     hero.style.left=(startpositionleft-1) * 48 + "px";
     hero.style.top=(startpositiontop-1) * 48 + "px";
-    hero.posx = 2;
-    hero.posy = 2;
+    hero.posx = startpositionleft-1;
+    hero.posy = startpositiontop-1;
     document.getElementById("div-hero").appendChild(hero);
   }, 100);
 
